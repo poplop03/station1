413062a5a2e37f8a63a363520684893b363413e0
duc <thienductang@gmail.com>
2025-06-12 12:32:08 +0700

    ignore

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/duc/ROS1/station1/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/duc/ROS1/station1/devel;/opt/ros/noetic'.split(';') if '/home/duc/ROS1/station1/devel;/opt/ros/noetic' != '' else []
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -148,6 +148,7 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/duc/ROS1/station1/build/gtest/cmake_install.cmake")
   include("/home/duc/ROS1/station1/build/station1_bringup/cmake_install.cmake")
   include("/home/duc/ROS1/station1/build/robot_description_v2/cmake_install.cmake")
+  include("/home/duc/ROS1/station1/build/robot_description_v3/cmake_install.cmake")
 
 endif()
 
